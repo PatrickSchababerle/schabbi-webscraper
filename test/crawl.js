b9308cca062f80e6523b6f2c15d43bb85d389f0f
Patrick Schababerle <patrick.schababerle@digitalsterne.de>
2021-04-08 14:35:27 +0200

    Update test

--- test/crawl.js
+++ test/crawl.js
@@ -2,7 +2,7 @@ const Schabbi = require('./../Schabbi.js');
 const Crawler = new Schabbi();
 
 Crawler
-    .setUrl('https://www.digitalsterne.de')
+    .setUrl('https://www.example.com')
     .withOptions({
         includeExternalLinks : true,
         userAgent : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
